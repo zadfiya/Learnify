ab8fc2d4d7f947ad8ce6cd1954fd35746e79d7ca
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-05-25 13:00:56 -0400

    Routes:Teacher Created nd middleware updated

--- Backend/middleware/custom-middleware.js
+++ Backend/middleware/custom-middleware.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose')
 
 module.exports = {
   isLoggedIn: (req, res, next) => req.isAuthenticated() ? next() : res.status(403).json({ message: 'Log in to access' }),
-  isValidId: (req, res, next) => mongoose.Types.ObjectId.isValid(req.params.id) ? next() : res.status(404).json({ message: 'Invalid ID' })
+  isValidId: (req, res, next) => mongoose.Types.ObjectId.isValid(req.params.id) ? next() : res.status(404).json({ message: 'Invalid ID' }),
+  isTeacher: (req, res, next) => req.user.role === 'Teacher' ? next() : res.status(403).json({ message: 'Unauthorized' }),
 }
