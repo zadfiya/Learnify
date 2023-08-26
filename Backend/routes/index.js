35c24e7765847e205a4ea94f67ebc88060e15a3a
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-08-25 21:02:26 -0400

    User and Teacher routes added

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,4 +1,6 @@
 module.exports = app => {
 
+    app.use('/api/teachers', require('./teachers.routes.js'))
+    app.use('/api/users', require('./users.routes.js'))
     app.use('/api', require('./auth.routes.js'))
 }
